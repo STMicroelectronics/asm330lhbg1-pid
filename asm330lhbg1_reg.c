8b1aa7a90938e5d245f97ccefe81cca2fd568aa4
Armando Visconti <armando.visconti@st.com>
2024-11-14 11:37:47 +0100

    Fix fifo_watermark_set() API
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- asm330lhbg1_reg.c
+++ asm330lhbg1_reg.c
@@ -4493,19 +4493,15 @@ int32_t asm330lhbg1_fifo_watermark_set(const stmdev_ctx_t *ctx, uint16_t val)
   asm330lhbg1_fifo_ctrl2_t fifo_ctrl2;
   int32_t ret;
 
-  ret = asm330lhbg1_read_reg(ctx, ASM330LHBG1_FIFO_CTRL2,
-                             (uint8_t *)&fifo_ctrl2, 1);
+  ret = asm330lhbg1_read_reg(ctx, ASM330LHBG1_FIFO_CTRL1, (uint8_t *)&fifo_ctrl1, 1);
+  ret += asm330lhbg1_read_reg(ctx, ASM330LHBG1_FIFO_CTRL2, (uint8_t *)&fifo_ctrl2, 1);
+
   if (ret == 0)
   {
+    fifo_ctrl1.wtm = (uint8_t)(val  & 0xFFU);
     fifo_ctrl2.wtm = (uint8_t)((val / 256U) & 0x01U);
-    ret = asm330lhbg1_write_reg(ctx, ASM330LHBG1_FIFO_CTRL2,
-                                (uint8_t *)&fifo_ctrl2, 1);
-  }
-  if (ret == 0)
-  {
-    fifo_ctrl1.wtm = (uint8_t)(val - (fifo_ctrl2.wtm * 256U));
-    ret = asm330lhbg1_write_reg(ctx, ASM330LHBG1_FIFO_CTRL1,
-                                (uint8_t *)&fifo_ctrl1, 1);
+    ret = asm330lhbg1_write_reg(ctx, ASM330LHBG1_FIFO_CTRL1, (uint8_t *)&fifo_ctrl1, 1);
+    ret += asm330lhbg1_write_reg(ctx, ASM330LHBG1_FIFO_CTRL2, (uint8_t *)&fifo_ctrl2, 1);
   }
 
   return ret;
